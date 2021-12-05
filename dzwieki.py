af2ba5c2dd3f09b62180369434c98fbb9665c01a
tomasz <tomasz.pionka@protonmail.com>
2021-12-05 17:02:18 +0100

    Formatted scripts with black

--- dzwieki.py
+++ dzwieki.py
@@ -6,55 +6,58 @@ import time
 from transformers import pipeline
 from translate import Translator
 
-#Niezrozumienie
+# Niezrozumienie
 def unrecognized():
-    text='Nie rozumiem, powtórz proszę.'
-    language = 'pl'
+    text = "Nie rozumiem, powtórz proszę."
+    language = "pl"
     speech = gTTS(text=text, lang=language, slow=False)
-    speech.save('text.mp3')
-    music = pyglet.media.load('text.mp3', streaming=False)
+    speech.save("text.mp3")
+    music = pyglet.media.load("text.mp3", streaming=False)
     music.play()
     time.sleep(music.duration)
-    os.remove('text.mp3')
+    os.remove("text.mp3")
 
-#Nagrywanie tekstu
+
+# Nagrywanie tekstu
 def record_voice():
-    r=sr.Recognizer()
+    r = sr.Recognizer()
     mic = sr.Microphone()
-    print('start')
+    print("start")
     with mic as source:
         r.adjust_for_ambient_noise(source)
         audio = r.listen(source, timeout=10)
-    print('koniec')
-    return str(r.recognize_google(audio, language='pl-PL'))
+    print("koniec")
+    return str(r.recognize_google(audio, language="pl-PL"))
+
 
 def speaker(moj_tekst):
-    speech=gTTS(text=moj_tekst,lang='pl', slow = False)
-    speech.save('moja_mowa.mp3')
-    music = pyglet.media.load('moja_mowa.mp3', streaming=False)
+    speech = gTTS(text=moj_tekst, lang="pl", slow=False)
+    speech.save("moja_mowa.mp3")
+    music = pyglet.media.load("moja_mowa.mp3", streaming=False)
     music.play()
     time.sleep(music.duration)
-    os.remove('moja_mowa.mp3')
+    os.remove("moja_mowa.mp3")
+
 
 def yes_no(tekst):
-    translator = Translator(to_lang='en', from_lang='pl')
+    translator = Translator(to_lang="en", from_lang="pl")
     translation = str(translator.translate(tekst))
     print(tekst)
-    classifier = pipeline('zero-shot-classification')
+    classifier = pipeline("zero-shot-classification")
 
-    wynik=classifier(
+    wynik = classifier(
         translation,
-        candidate_labels = ['yes','no'],  
+        candidate_labels=["yes", "no"],
     )
-    TLUMACZ = {'yes':True,
-                'no':False}
-    scores = wynik['scores']
-    labele = wynik['labels']
-    res = dict(zip(labele,scores))
+    TLUMACZ = {"yes": True, "no": False}
+    scores = wynik["scores"]
+    labele = wynik["labels"]
+    res = dict(zip(labele, scores))
     max_key = max(res, key=res.get)
     return TLUMACZ[max_key]
 
-def asking(text,topic_function):
+
+def asking(text, topic_function):
     speaker(text)
     while True:
         wynik = topic_function()
@@ -70,4 +73,3 @@ def asking(text,topic_function):
         else:
             speaker(text)
     return wynik
-
